dd967e4f712c893c1d72df1dd5bff0570fe2eafd
kgwkgwkgw <kgwaaa@naver.com>
2020-03-09 02:16:32 +0900

    [api] 불필요 코드 삭제 및 주석 추가

--- api/src/main/java/com/dasangwan/pickit/api/config/H2ServerConfiguration.java
+++ api/src/main/java/com/dasangwan/pickit/api/config/H2ServerConfiguration.java
@@ -13,11 +13,6 @@ import java.sql.SQLException;
 @Profile("local")
 public class H2ServerConfiguration {
     @Bean
-    public Server h2TcpServer() throws SQLException {
-        return Server.createTcpServer()
-                .start();
-    }
-    @Bean
     @ConfigurationProperties("spring.datasource") // yml의 설정값을 Set한다.
     public DataSource dataSource() throws SQLException {
         Server.createTcpServer("-ifNotExists", "-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
